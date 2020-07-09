f2696a45c28002d14aebfed5bd5cb241cf40708c
Vitaly Prosvetov <vp12@yandex.ru>
2020-07-09 16:46:58 +0300

    first commit

---
+++ SwiftBaseCourse/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftBaseCourse
+//
+//  Created by Vitaly Prosvetov on 09.07.2020.
+//  Copyright © 2020 Vitaly Prosvetov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
