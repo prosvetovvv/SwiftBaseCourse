631476b8f3907d00eed262f8e673ad7fddcc7270
Vitaly Prosvetov <vp12@yandex.ru>
2020-07-16 11:03:28 +0300

    Add home work for lesson 6.

--- SwiftBaseCourse/main.swift
+++ SwiftBaseCourse/main.swift
@@ -8,21 +8,69 @@
 
 import Foundation
 
+// Lesson 6
+
+// Create objects with type MuscleCar and TankCar
+
+var muscleCar1 = MuscleCar(name: .BMW, muscleCarVin: "11111", muscleCarColor: .Black, odometer: 24500)
+var muscleCar2 = MuscleCar(name: .Ford, muscleCarVin: "22222", muscleCarColor: .Blue, odometer: 8700)
+var muscleCar3 = MuscleCar(name: .Subaru, muscleCarVin: "33333", muscleCarColor: .Green, odometer: 23000)
+
+var tankCar1 = TankCar(name: .Man, tankCarVin: "44444", tankCarColor: .Blue, odometer: 450000)
+var tankCar2 = TankCar(name: .Scania, tankCarVin: "55555", tankCarColor: .Green, odometer: 340000)
+var tankCar3 = TankCar(name: .Volvo, tankCarVin: "66666", tankCarColor: .Red, odometer: 50000)
+var tankCar4 = TankCar(name: .Man, tankCarVin: "77777", tankCarColor: .Green, odometer: 43000)
+
+// Create objects with type Queue<T>
+
+var muscleQueue = Queue<MuscleCar>()
+var tankQueue = Queue<TankCar>()
+
+// Insert elements in to queue
+
+muscleQueue.enqueue(muscleCar1)
+muscleQueue.enqueue(muscleCar2)
+muscleQueue.enqueue(muscleCar3)
+
+tankQueue.enqueue(tankCar1)
+tankQueue.enqueue(tankCar2)
+tankQueue.enqueue(tankCar3)
+tankQueue.enqueue(tankCar4)
+
+// Pull element from queue
+
+if let q = tankQueue.dequeue() {
+    print(q)    
+}
+print("================")
+
+// Return "nil" if index out of range
+
+print(tankQueue[8])
+
+print("================")
+
+// Call method higher order with closure
+
+let filterTankQueue = tankQueue.filterByOdometer {
+    $0.odometer <= 400000
+}
+print(filterTankQueue)
+
+
 // Lesson 5
 
-var muscleCar1 = MuscleCar(name: .BMW, muscleCarVin: "11111", muscleCarColor: .Black)
-var tankCar1 = TankCar(name: .Man, tankCarVin: "22222", tankCarColor: .Green)
+//var muscleCar1 = MuscleCar(name: .BMW, muscleCarVin: "11111", muscleCarColor: .Black)
+//var tankCar1 = TankCar(name: .Man, tankCarVin: "22222", tankCarColor: .Green)
 
 
-print(muscleCar1.rePaint(to: .Green))
-print(muscleCar1.openWindow())
-
-print(tankCar1.loadTank())
-
-print(muscleCar1.description)
-print(tankCar1.description)
-
-
+//print(muscleCar1.rePaint(to: .Green))
+//print(muscleCar1.openWindow())
+//
+//print(tankCar1.loadTank())
+//
+//print(muscleCar1.description)
+//print(tankCar1.description)
 
 
 // Lesson 4
