bcaa29df2df0a905e25c9f470582b55878c24615
Vitaly Prosvetov <vp12@yandex.ru>
2020-07-23 13:10:57 +0300

    Lesson8_Snake_SpriteKit

---
+++ Lesson8_Snake/Lesson8_Snake/Models/SnakeHead.swift
@@ -0,0 +1,23 @@
+//
+//  SnakeHead.swift
+//  Lesson8_Snake
+//
+//  Created by Vitaly Prosvetov on 22.07.2020.
+//  Copyright © 2020 Vitaly Prosvetov. All rights reserved.
+//
+
+import UIKit
+
+class SnakeHead: SnakeBodyPart {
+    override init(atPoint point: CGPoint){
+        super.init(atPoint:point)
+        // категория - голова
+        self.physicsBody?.categoryBitMask = CollisionCategories.SnakeHead
+        // пересекается с телом, яблоком и границей экрана
+        self.physicsBody?.contactTestBitMask = CollisionCategories.EdgeBody | CollisionCategories.Apple | CollisionCategories.Snake
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
